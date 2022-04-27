13f82ace40b79a2d2571a7f9f89e89414302f999
duhuazhu <842296411@qq.com>
2022-04-27 11:55:26 +0800

    学习java

---
+++ code/day01/HelloWorld.java
@@ -0,0 +1,14 @@
+/** 文档注释
+ * @author duhuazhu
+ * @version v1.0
+ */
+
+public class HelloChina{
+    /**
+     * 下面按是main方法  main方法的格式程序格式是固定的
+     *
+     * */
+        public static void main(String[] args){
+            System.out.println("Hello World");
+        }
+ }
