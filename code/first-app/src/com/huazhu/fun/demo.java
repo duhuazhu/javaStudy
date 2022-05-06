da51cfe3d7201c212ebdd33b2c7a601b545bc5f2
“duhuazhu” <335921101@qq.com>
2022-05-06 21:50:36 +0800

    java学习

---
+++ code/first-app/src/com/huazhu/fun/demo.java
@@ -0,0 +1,23 @@
+package com.huazhu.fun;
+
+import java.util.Scanner;
+
+public class demo {
+    public static int add(int a, int b) {
+
+        return a + b;
+
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(add(10, 20));
+        print();
+    }
+
+    public static void print() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println("ÄãºÃ°¡");
+        }
+    }
+
+}
