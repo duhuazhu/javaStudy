03e15100198dbae7b7cbec8dc985c3b56ef1251b
“duhuazhu” <335921101@qq.com>
2022-05-30 22:03:44 +0800

    java 泛型定义

---
+++ code/opo-demo-day01/src/com/huazhuInterface/teacher.java
@@ -0,0 +1,24 @@
+package com.huazhuInterface;
+
+public class teacher implements data<teacher> {
+
+    @Override
+    public void add(teacher teacher) {
+
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public void update(teacher teacher) {
+
+    }
+
+    @Override
+    public teacher queryById(int id) {
+        return null;
+    }
+}
