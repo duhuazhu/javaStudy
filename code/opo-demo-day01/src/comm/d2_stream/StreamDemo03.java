ee3c19cdfc17798f0f7e88cdd21e34db5bc36ca7
duhuazhu <335921101@qq.com>
2022-07-28 17:23:26 +0800

    stream 03 课程 学习

---
+++ code/opo-demo-day01/src/comm/d2_stream/StreamDemo03.java
@@ -0,0 +1,15 @@
+package comm.d2_stream;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamDemo03 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("张无忌");
+        list.add("周芷若");
+        list.add("五峰");
+        list.add("乔峰");
+        list.add("虚竹");
+    }
+}
