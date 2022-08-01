678990b3cade36ee8d3f1e1eeefae2c2830e6cac
duhuazhu <335921101@qq.com>
2022-08-01 14:10:24 +0800

    Exception 异常抛出

--- code/opo-demo-day01/src/d3_exception/ExceptionDemo.java
+++ code/opo-demo-day01/src/d3_exception/ExceptionDemo.java
@@ -1,8 +1,32 @@
 package d3_exception;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ExceptionDemo {
-    public static void main(String[] args) {
-        int [] arr = {10,20,30};
+    // throw Exception
+    public static void main(String[] args)   {
+        System.out.println("程序开始......");
+
+        try {
+            parseTime("2011-11-11 11:11:11");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("程序结束......");
 
     }
+
+    public static void parseTime(String date) throws Exception {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date d = sdf.parse(date);
+            System.out.println(d);
+
+            InputStream is = new FileInputStream("E:/meinv.jpg");
+    }
+
 }
